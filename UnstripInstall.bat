31bc8d7d989b0b168cc4e88571c97e49ec8b00c3
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2022-12-18 10:09:18 -0800

    Add files via upload

---
+++ UnstripInstall.bat
@@ -0,0 +1,18 @@
+@echo off
+if not exist "UnstrippedLibs" mkdir UnstrippedLibs
+curl -L -o "temp1.zip" https://unity.bepinex.dev/libraries/2019.3.15.zip
+curl -L -o "temp2.zip" https://unity.bepinex.dev/corlibs/2019.3.15.zip
+powershell -command "Expand-Archive -Force '%~dp0temp1.zip' '%~dp0UnstrippedLibs'
+powershell -command "Expand-Archive -Force '%~dp0temp2.zip' '%~dp0UnstrippedLibs'
+del "temp1.zip"
+del "temp2.zip"
+set "file=%~dp0doorstop_config.ini"
+set "file2=%~dp0temp.ini"
+:Replace
+>"%file2%" (
+  for /f "usebackq delims=" %%A in ("%file%") do (
+    if "%%A" equ "dllSearchPathOverride=" (echo dllSearchPathOverride=UnstrippedLibs) else (echo %%A)
+  )
+)
+del "%~dp0doorstop_config.ini"
+ren "temp.ini" "doorstop_config.ini"
